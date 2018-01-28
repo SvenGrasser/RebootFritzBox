925d71175bc524b732000c4f8ac5b1f56f9a154d
Sven Grasser <sven.grasser@gmail.com>
2018-01-28 11:20:15 +0100

    options moved to property-file; Property class implemented

--- rebootFritzBox.py
+++ rebootFritzBox.py
@@ -2,42 +2,44 @@ import time
 import os
 import yaml;
 
-properties = 'rebootFritzBox.properties.yml'
-timerTick = 60
-rebootTime = '16:50'
-isRunning = True
-executeReboot = True
+class Property:
+    propertyFile = 'rebootFritzBox.properties.yml'
+    iRunning = False
+    executeReboot = False
+    rebootTime = ''
+    password = ''
+    def __init__(self):
+        properties_path = os.path.dirname(os.path.realpath(__file__))
+        properties_fullpath = os.path.join(properties_path, self.propertyFile)
+        with open(properties_fullpath, 'r') as properties_file:
+            prop = yaml.load(properties_file)
+            self.isRunning = prop['options']['isRunning']   
+            self.executeReboot = prop['options']['executeReboot']     
+            self.rebootTime = prop['options']['reboot']    
+            self.password = prop['authentication']['password']
 
-def isRebootTime():
+def isRebootTime(p):
     now = time.strftime('%H:%M')
     print ('check RebootTime: ', now)
-    if now == rebootTime:
+    if now == p.rebootTime:
         return True
     else:
         return False
 
-def getPassword():
-    properties_path = os.path.dirname(os.path.realpath(__file__))
-    properties_fullpath = os.path.join(properties_path, properties)
-    with open(properties_fullpath, 'r') as properties_file:
-        prop = yaml.load(properties_file)    
-        return prop['authentification']['password']
-    return ''
-
-def reboot():
+def reboot(p):
     print ('reboot FritzBox: ', time.strftime('%X %x'))
-    pw = getPassword()
-    if executeReboot == True:
+    if p.executeReboot == True:
         from fritzconnection import FritzConnection
-        connection = FritzConnection(password=pw)
+        connection = FritzConnection(password=p.password)
         connection.call_action('DeviceConfig', 'Reboot')
-    return;
 
 def run():
-    print('reboot FritzBox: service is running')
-    while isRunning == True:
-        if isRebootTime():
-            reboot()
-        time.sleep(timerTick)
+    print('reboot FritzBox: service is running')  
+    
+    p = Property()
+    while p.isRunning == True:
+        if isRebootTime(p):
+            reboot(p)
+        time.sleep(60)
 
 run()
